406d143af280baad61cd50c3fe43459461681b69
Bilge-Su <95489568+Bilge-Su@users.noreply.github.com>
2024-03-02 18:39:48 +0300

    Solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if(num<0) return "ERROR";
+    result = "";
+    let i = 0;
+    while(i<num){
+        result += string;
+        i += 1; }
+    return result;
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
